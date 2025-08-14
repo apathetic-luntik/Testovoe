21cbbbcc20cbc8de8e171b1d6b5936e764c6ae20
Stasai Mtv <mtv-an24@yandex.ru>
2025-08-14 07:17:47 +0300

    удален build из репозитория

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_apptest5_FILE C:/Users/Admin/Documents/allTestovoe/Testovoe/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/apptest5.exe)
-set(__QT_DEPLOY_TARGET_apptest5_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_apptest5_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6QmlMeta.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6QmlWorkerScript.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
